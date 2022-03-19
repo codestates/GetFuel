306c764e9a7007a43e31eedce87494d9c69bf3ff
Daekuen <daekuenhan@gmail.com>
2022-03-19 22:36:40 +0900

    deleteAccount api 구현

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,7 +9,7 @@ router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 router.get('/refresh', authController.refresh);
 router.get('/signout', isAuth, authController.signout);
-router.delete('/deleteaccount', );
+router.delete('/deleteaccount/:id', isAuth, authController.deleteAccount);
 
 
 export default router;
