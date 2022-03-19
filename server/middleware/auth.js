bbf51906785732c69ab506a9c9c9216cfd1596a7
Daekuen <daekuenhan@gmail.com>
2022-03-19 19:01:02 +0900

    middleware/isAuth 추가

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,5 +4,26 @@ import * as usersRepository from '../data/auth.js';
 
 export const isAuth = async (req, res, next) => {
 
+  const authHeader = req.get('Authorization')
+  if(!(authHeader && authHeader.startsWith('Bearer '))) {
+    return res.status(401).json({ message: 'Authentication Error' })
+  }
+
+  const token = authHeader.split(' ')[1]; //access token
+
+
+  jwt.verify(token, config.jwt.access_secret, async(error, decoded) => {
+    if(error) {
+      return res.status(401).json( { message: 'Authentication Error' } )
+    };
+  
+    const user = await usersRepository.findById(decoded.id);
+    if(!user) {
+      return res.status(401).json( { message: 'Authentication Error' })
+    }
+
+    req.userId = user.id // req에 userId 추가 (custom)
+    next();
+  })
 
 } 
