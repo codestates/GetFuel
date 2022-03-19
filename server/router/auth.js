937f71439214d4b07616cf09022d7d14e24872c2
Daekuen <daekuenhan@gmail.comq>
2022-03-19 21:52:48 +0900

    logout api 구현

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 import * as authController from '../controller/auth.js'
+import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 router.get('/refresh', authController.refresh);
-router.post('/singout', );
+router.get('/signout', isAuth, authController.signout);
 router.delete('/deleteaccount', );
 
 
