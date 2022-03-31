5166c9e507e8ebc870868ab01b40b8076238ad29
wjdgns950621 <87626549+wjdgns950621@users.noreply.github.com>
2022-03-31 20:24:15 +0900

    Feat: review page (#63)
    * 초기설정
    
    * 초기설정.
    
    * 깃연습
    
    * 깃연습
    
    * 메인페이지,로그인
    
    * 로그인기능 구현
    
    * 로그인기능 구현
    
    * 리뷰페이지구현
    
    * 푸쉬충돌해결2
    
    * 푸쉬충돌해결3
    
    Co-authored-by: Best-engineer <kkm6101@gmail.com>

---
+++ client/src/Page/Map.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styles from './Map.module.css'
+import Nav from '../Component/Nav'
+
+
+class Map extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+render() {
+    return (
+        <div>맵이요</div>
+    )
+}
+}
+
+export default Map;
