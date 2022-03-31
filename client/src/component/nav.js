3193792213845082ce6ff9496c7c4db42999132b
wjdgns950621 <wjdgns950621@gmail.com>
2022-03-31 11:35:43 +0900

    푸쉬충돌해결2

--- client/src/component/nav.js
+++ client/src/component/nav.js
@@ -1,25 +1,14 @@
 import React from 'react'
-<<<<<<< HEAD
 import styles from './Nav.module.css'
-=======
-import './nav.css'
->>>>>>> c9b8d8d1c90ddf63f80f21002627f1f7a0364802
 import GetFuel from '../GetFuel_logo.png'
 
 
 function Nav () {
     return (
-<<<<<<< HEAD
         <div className={styles.nav}>
                 <img className={styles.logo} src={GetFuel} />
                 <div className={styles.menu}>
                 <a href='signup'>Sign up</a>
-=======
-        <div className='nav'>
-                <img className='logo' src={GetFuel} />
-                <div className='menu'>
-                <a href=''>Sign up</a>
->>>>>>> c9b8d8d1c90ddf63f80f21002627f1f7a0364802
                 <a href='login'>Login</a>
                 </div>
             </div>
