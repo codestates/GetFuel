95ac4ac95e68768dd2856fc2a7d35bdad9ba5750
Daekuen <daekuenhan@gmail.com>
2022-04-01 18:48:50 +0900

    Feat: init Kakao map

--- client/src/Page/MapContainer.js
+++ client/src/Page/MapContainer.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+/* global kakao */
+import React, { useState, useEffect } from 'react';
 import SearchBar from '../component/SearchBar.js';
+
+const { kakao } = window;
 
 const MapContainer = () => {
   const [searchValue, setSearchValue] = useState('서울시청');
-  console.log(searchValue);
+  const [kakaoMap, setKakaoMap] = useState(null);
+  const [centerCoordi, setCenterCoordi] = useState([
+    37.56683690482874, 126.9786564967784,
+  ]);
+
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(centerCoordi[0], centerCoordi[1]),
+      level: 7,
+    };
+    const map = new kakao.maps.Map(container, options);
+    setKakaoMap(map); //여기까지가 지도 생성
+  }, []);
+
   return (
     <div>
       <SearchBar setSearchValue={setSearchValue} />
+      <div id="map" style={{ width: '100%', height: '585px' }}></div>
     </div>
   );
 };
