ee9c3fe8b8f7e6b85d22d8645c58861ed6eddb8c
JungHoon0814 <57275302+JungHoon0814@users.noreply.github.com>
2022-04-02 17:11:55 +0900

    Feat: make signup, edituser page & code conflict fix (#91)
    * 초기설정
    
    * Update README.md
    
    * Update README.md
    
    * signuppage
    
    * 회원가입,회원정보 수정 구현
    
    * recommit
    
    * dev branch pull & conflicts fix
    
    * conflict fix & add Signup, Edituser page
    
    Co-authored-by: Best-engineer <kkm6101@gmail.com>
    Co-authored-by: Best-engineer <85835389+Best-engineer@users.noreply.github.com>

---
+++ client/src/components/userinformationform/UserInformationForm.js
@@ -0,0 +1,27 @@
+import React,{ useState } from 'react';
+import styles from './UserInformationForm.module.css';
+
+function UserInformationForm() {
+  // 회원가입 정보 데이터베이스에 넘겨줄 코드
+   
+
+    return (
+    
+      <div>
+        <form className={styles.inputform}>
+            <div className={styles.email}>email</div>
+              <input className={styles.userinfo} type="text" placeholder='이메일을 입력하세요'/>
+            <div className={styles.nickname}>nickname</div>
+              <input className={styles.userinfo} type='text' placeholder='사용할 닉네임을입력하세요' />
+            <div className={styles.password}>password</div>
+              <input className={styles.userinfo} type='password' placeholder='비밀번호를 입력하세요' />
+            <div className={styles.reenterpassword}>reenterpassword</div>
+              <input className={styles.userinfo} type='password' placeholder='비밀번호 확인'/> 
+        </form>
+        
+      </div>
+
+    );
+};
+
+export default UserInformationForm;
