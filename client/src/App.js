efbfc4d86514911c64a3a230c1e582c321e01bda
Daekuen <86470018+Daekuen@users.noreply.github.com>
2022-04-19 09:28:22 +0900

    Revert "Feat: review page nav 구현"

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,6 @@ export default function App({ opinet }) {
     loginHandler,
     issueAccessToken,
   });
-  console.log(isLogin)
   const axiosInstance = useAxiosPrivate(userInfo?.accessToken, loginFunctions); // custom axios 객체;
 
   useEffect(async () => {
@@ -83,8 +82,8 @@ export default function App({ opinet }) {
             setIsLogin={setIsLogin}
           />
         </Route>
-        <Route path="/review">
-          <Review axiosInstance={axiosInstance} userInfo={userInfo} setIsLogin={setIsLogin} />
+        <Route path='/review'>
+          <Review axiosInstance={axiosInstance} userInfo={userInfo} />
         </Route>
         <Route path='/signup' component={SignUp} />
         <Route path='/edituser'>
