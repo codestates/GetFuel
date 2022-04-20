b6559da72e279ba96a43f59767adf6d205e3eb00
wjdgns950621 <wjdgns950621@gmail.com>
2022-04-20 13:24:42 +0900

    Fix: menu hover add

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ export default function App({ opinet }) {
     loginHandler,
     issueAccessToken,
   });
-  console.log(isLogin)
+  
   const axiosInstance = useAxiosPrivate(userInfo?.accessToken, loginFunctions); // custom axios 객체;
 
   useEffect(async () => {
